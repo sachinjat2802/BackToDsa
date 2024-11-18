3b3c067291519fd6a8dcbb4658d7acaf171e5fa0
sachin jat <53407215+sachinjat2802@users.noreply.github.com>
2024-11-18 12:43:25 +0000

    BIG O(N^2)

--- OofN^2.js
+++ OofN^2.js
@@ -1,13 +1,12 @@
 function logIteams(n){
     let counter =0
     for(let i =0 ; i<=n;i++){
-        console.log(i)
-        counter++
+        for(let j =0 ; j<=n;j++){
+            console.log(i,j)
+            counter++
+        }
     }
-    for(let j =0 ; j<=n;j++){
-        console.log(j)
-        counter++
-    }
+    
     console.log("counter", counter)
 }
 
